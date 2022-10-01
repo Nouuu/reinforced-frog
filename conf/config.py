3f4ed0a153c12b5b527ed96c5f46adeda4f05946
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2022-10-01 14:49:09 +0200

    update sprites

--- conf/config.py
+++ conf/config.py
@@ -74,17 +74,17 @@ def get_sprite_local(name: str, sprite_size: float = 0.5):
 # WORLD ENTITIES
 
 ENTITIES: {str: WorldEntity} = {
-    CAR_TOKEN: WorldEntity(1, 1, CAR_TOKEN, get_sprite_local("car_1", 0.6)),
+    CAR_TOKEN: WorldEntity(1, 1, CAR_TOKEN, get_sprite_local("car_1", 0.65)),
     EXIT_TOKEN: WorldEntity(1, 1, EXIT_TOKEN, get_sprite_local('grass', 2)),
     FROG_TOKEN: WorldEntity(1, 1, FROG_TOKEN, get_sprite_local('frog', 0.12)),
     GROUND_TOKEN: WorldEntity(1, 1, GROUND_TOKEN, get_sprite_resources('tiles/stoneCenter')),
     ROAD_TOKEN: WorldEntity(1, 1, ROAD_TOKEN, get_sprite_resources('topdown_tanks/tileGrass_roadEast', 1)),
     START_TOKEN: WorldEntity(1, 1, START_TOKEN, get_sprite_local('grass', 2)),
-    TRUCK_TOKEN: WorldEntity(2, 1, TRUCK_TOKEN, get_sprite_local("truck")),
-    TURTLE_TOKEN: WorldEntity(1, 1, TURTLE_TOKEN, get_sprite_local("turtle", 0.1)),
+    TRUCK_TOKEN: WorldEntity(2, 1, TRUCK_TOKEN, get_sprite_local("truck", 0.32)),
+    TURTLE_TOKEN: WorldEntity(1, 1, TURTLE_TOKEN, get_sprite_local("turtle", 0.2)),
     WALL_TOKEN: WorldEntity(1, 1, WALL_TOKEN, get_sprite_resources('tiles/stoneCenter')),
     WATER_TOKEN: WorldEntity(1, 1, WATER_TOKEN, get_sprite_resources('tiles/water')),
-    WOOD_TOKEN: WorldEntity(1, 1, WOOD_TOKEN, get_sprite_local("wood", 0.15)),
+    WOOD_TOKEN: WorldEntity(1, 1, WOOD_TOKEN, get_sprite_local("wood", 0.21)),
 }
 
 # WORLD
@@ -107,10 +107,10 @@ WORLD_ENTITIES = {
     (13, 18): ENTITIES[TURTLE_TOKEN],
     (13, 30): ENTITIES[TURTLE_TOKEN],
     (13, 50): ENTITIES[WOOD_TOKEN],
-    (13, 56): ENTITIES[WOOD_TOKEN],
+    (13, 59): ENTITIES[WOOD_TOKEN],
     (31, 4): ENTITIES[TRUCK_TOKEN],
     (31, 15): ENTITIES[CAR_TOKEN],
-    (31, 26): ENTITIES[CAR_TOKEN],
+    (31, 24): ENTITIES[CAR_TOKEN],
     (31, 60): ENTITIES[CAR_TOKEN],
     (40, 20): ENTITIES[TRUCK_TOKEN],
     (40, 70): ENTITIES[CAR_TOKEN],
