4cf7002c0ec7444b355a7e1c916c2fc8c937adb3
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2022-10-13 08:43:19 +0200

    update config learning & debug

--- game/game.py
+++ game/game.py
@@ -46,19 +46,15 @@ class Game:
                 player.world_entity
             )
             player.step(action, reward, new_state, environment)
-            if self.__i % 100 == 0 and not player.is_human and self.__debug:
-                print(
-                    f"Score : {round(player.score, 4)}, \tlast state : {new_state}, q : {player.get_qtable_state(environment, new_state)}")
+            # if self.__i % 100 == 0 and not player.is_human and self.__debug:
+            #     print(
+            #         f"Score : {round(player.score, 4)}, \tlast state : {new_state}, q : {player.get_qtable_state(environment, new_state)}")
 
             game_over = game_over or is_game_over
-            # if is_game_over:
-            #     if player.score > 0:
-            #         print(f"WIN ! score : {player.score}")
-            #     else:
-            #         print(f"LOSE ! score : {player.score}")
             if is_game_over:
                 if self.__debug:
-                    print(f"Score : {round(player.score, 4)},\t\tlast state : {new_state}")
+                    print(
+                        f"{'WIN' if player.score > 0 else 'LOSE'} ! Score : {round(player.score, 4)},\t\tlast state : {new_state}")
                 player.save_score()
                 if self.__auto_start:
                     self.init_player(player)
