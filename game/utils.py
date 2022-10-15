7d7f536eb00d0ec7d9d1c6a157bd3adeba34d949
pooetitu <pooetitu@gmail.com>
2022-10-15 12:33:46 +0200

    Optimize get collisions function

--- game/utils.py
+++ game/utils.py
@@ -14,12 +14,9 @@ from display.entity.world_entity import WorldEntity
 
 
 def get_collisions(entity: WorldEntity, state: tuple, world_entity_matrix: list[list[str]], scaling: int) -> [tuple]:
-    collisions = set()
     entity_min_x = state[1]
     entity_max_x = min(state[1] + entity.width * scaling, len(world_entity_matrix[0]))
-    for i in range(entity_min_x, entity_max_x):
-        collisions.add(world_entity_matrix[state[0]][i])
-    return collisions
+    return set(world_entity_matrix[state[0]][entity_min_x:entity_max_x])
 
 
 def is_in_safe_zone_on_water(entity: WorldEntity, entity_state: tuple, world_entity_matrix: list[list[str]],
