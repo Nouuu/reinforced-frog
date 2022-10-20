9127bab4ce460759c31539d39b98c41598009c52
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2022-10-20 16:18:47 +0200

    add model interface

---
+++ ai/Model.py
@@ -0,0 +1,21 @@
+from typing import Dict
+
+
+class Model:
+    def load(self, filename: str):
+        pass
+
+    def save(self, qtable_filename: str, score_filename: str):
+        pass
+
+    def get_state_actions(self, qtable: dict, state: [str]) -> Dict[str, float]:
+        pass
+
+    def update_state(self, state: [str], max_q: float, reward: float, action: str):
+        pass
+
+    def print_stats(self, time_elapsed: int):
+        pass
+
+    def save_score(self, score: float):
+        pass
