6e100b93d487995f667891c37cdb4ef5f8306ef2
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2022-10-22 20:14:57 +0200

    update world & qtable config

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import time
 import arcade
 
 from ai.Agent import Agent
+from ai.DQtable import DeepQtable
 from ai.Model import Model
 from ai.Qtable import Qtable
 from ai.graph_exporter import extract_history
@@ -24,21 +25,15 @@ def main():
         world_lines=WORLD_LINES[env['WORLD_TYPE']],
         env=env)
 
-    # dqtable = DeepQtable(score_history_packets=env['QTABLE_HISTORY_PACKETS'],
-    #                      visible_lines_above=env['AGENT_VISIBLE_LINES_ABOVE'],
-    #                      visible_cols_arround=env['AGENT_VISIBLE_COLS_ARROUND'],
-    #                      agent_width=9,
-    #                      reward_divisor=WORLD_WIDTH * WORLD_HEIGHT)
-    qtable = Qtable(float(env['AGENT_LEARNING_RATE']), float(env['AGENT_GAMMA']), env['QTABLE_HISTORY_PACKETS'],
-                    env['AGENT_VISIBLE_LINES_ABOVE'])
+    model = get_model(env)
 
     player = HumanPlayer()
     players = []
 
     for i in range(env['AGENT_COUNT']):
-        players.append(Agent(qtable, float(env['EXPLORE_RATE'])))
+        players.append(Agent(model, float(env['EXPLORE_RATE'])))
 
-    load_model(qtable, env)
+    load_model(model, env)
     if not env['LEARNING_MODE']:
         players.append(player)
         pass
@@ -47,11 +42,11 @@ def main():
 
     start_time = time.perf_counter()
     if env['LEARNING_MODE']:
-        learn_mode(qtable, env, game, start_time)
+        learn_mode(model, env, game, start_time)
     else:
         arcade_mode(game)
     if env['LEARNING_MODE']:
-        save_model(qtable, env)
+        save_model(model, env)
         if env['GENERATE_HISTORY_GRAPH']:
             extract_history(env['QTABLE_HISTORY_FILE'], env)
 
@@ -73,6 +68,20 @@ def arcade_mode(game):
     window = WorldWindow(game)
     window.setup()
     arcade.run()
+
+
+def get_model(env) -> Model:
+    if env['LEARNING_TYPE'] == 'QLEARNING':
+        return Qtable(float(env['AGENT_LEARNING_RATE']), float(env['AGENT_GAMMA']), env['QTABLE_HISTORY_PACKETS'],
+                      env['AGENT_VISIBLE_LINES_ABOVE'])
+    elif env['LEARNING_TYPE'] == 'DQLEARNING':
+        return DeepQtable(score_history_packets=env['QTABLE_HISTORY_PACKETS'],
+                          visible_lines_above=env['AGENT_VISIBLE_LINES_ABOVE'],
+                          visible_cols_arround=env['AGENT_VISIBLE_COLS_ARROUND'],
+                          agent_width=9,
+                          reward_divisor=WORLD_WIDTH * WORLD_HEIGHT)
+    print('Unknown learning type')
+    exit(1)
 
 
 def learn_mode(model: Model, env, game, start_time):
